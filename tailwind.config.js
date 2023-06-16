1402f950ca2ee56a8b7e3b337365acb5f572b3b4
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-16 10:20:29 +0200

    config tailwind with default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: "jit",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ["Poppins", "sans-serif"],
       },
+      colors: {
+        "primary-dark": {
+          DEFAULT: "#0E1234",
+          300: "#262942",
+          800: "#02051F",
+        },
+        "secondary-blue": {
+          DEFAULT: "#81E6D9",
+          300: "#A3FBEF",
+          800: "#72B9B0",
+        },
+        "accent-yellow": {
+          DEFAULT: "#FFDF38",
+          300: "#FFF09F",
+          800: "#E1BD00",
+        },
+      },
+      backgroundImage: {
+        'hero-bg': "url('/hero-bg.jpg')"
+      }
     },
   },
   plugins: [],
-}
+};
