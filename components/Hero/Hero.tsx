340ee43da262ade98556e5d6f227768c3cca9746
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-21 09:50:26 +0200

    add home introduction header

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Navbar from '../Navbar'
+
 
 const Hero = () => {
   return (
     <section>
-      <div className="min-h-screen flex items-center justify-center bg-primary-dark text-secondary-blue">
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-end bg-primary-dark text-secondary-blue">
         <div className="max-w-screen-lg px-4 py-8">
-
+          <div className="text-center">
+            <h1 className="text-4xl font-bold mb-4">Hi, I'm a web developer</h1>
+            {/* Other content */}
+          </div>
         </div>
       </div>
     </section>
