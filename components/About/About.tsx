271c756c02945d21d7e83124bfdf536f60546f26
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-21 09:08:45 +0200

    add wrappers to Hero and About

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section>
+      <div className="min-h-screen flex items-center justify-center bg-accent-yellow text-secondary-blue">
+        <div className="max-w-screen-lg px-4 py-8">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section>
+      <div className="min-h-screen flex items-center justify-center bg-primary-dark text-secondary-blue">
+        <div className="max-w-screen-lg px-4 py-8">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
