d72c13a0f0109f5ea9fb6859f0d512c53bda9e33
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-21 11:29:37 +0200

    update Hero text

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,8 +6,14 @@ const Hero = () => {
       <div className="min-h-screen flex items-center justify-end bg-primary-dark text-secondary-blue">
         <div className="max-w-screen-lg px-4 py-8">
           <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">Hi, I'm a web developer</h1>
-            {/* Other content */}
+            <h1 className="text-4xl font-bold mb-4">
+            <p>Hi there!</p>
+            <p>My name is Maciek</p>
+            <p>I’m a web developer</p>
+
+
+            </h1>
+            {/* move to next page img */}
           </div>
         </div>
       </div>
