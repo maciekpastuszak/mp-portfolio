7e549e73aac5124b13c7e39bd394918f2a1d8689
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-22 09:22:06 +0200

    change home link to icon svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import NavItem from './NavItem'
+import React from 'react';
+import NavItem from './NavItem';
+import HomeIcon from '../public/home-icon.svg';
 
-const navItems = ["Home", "About", "Portfolio", "Contact"]
+const navItems = [
+    { label: "Home", icon: <HomeIcon /> },
+    "About", 
+    "Portfolio", 
+    "Contact"]
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 flex items-center justify-between w-full p-4 bg-primary-dark text-secondary-blue z-10">
         <div className="flex w-1/2 items-center ms-6">
             <ul className="flex flex-row justify-between w-full">
-                {navItems.map((title, index) => (
+                {navItems.map((item, index) => (
                     <NavItem 
                         key={index} 
-                        title={title} 
+                        label={typeof item === 'object' ? item.label : item}
                     />
                 ))}
             </ul>
