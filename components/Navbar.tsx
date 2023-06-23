7764863b8242883c874315d2832493b86bc414c5
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-23 14:17:34 +0200

    update social icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavItem from './NavItem';
 import Image from 'next/image';
 import homeIcon from "../public/home-icon.svg";
-
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const navItems = [
     "About", 
@@ -33,8 +33,8 @@ const Navbar = () => {
                 <div className="flex items-center">
                     {/* SOCIAL MEDIA */}
                     <ul className="flex flex-row">
-                        <li>G</li>
-                        <li>L</li>
+                        <li className="text-secondary-blue ps-4"><BsGithub /></li>
+                        <li className="text-secondary-blue ps-4"><BsLinkedin /></li>
                     </ul>
                 </div>
                 </div>
