122f64344b92359c54074e7a9cdc39fb4ee83a03
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-23 23:22:47 +0200

    modify styles in hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,18 +3,20 @@ import React from 'react'
 const Hero = () => {
   return (
     <section>
-      <div className="min-h-screen flex items-center justify-end bg-primary-dark text-secondary-blue">
-        <div className="max-w-screen-lg px-4 py-8">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">
-            <p>Hi there!</p>
-            <p>My name is Maciek</p>
-            <p>I’m a web developer</p>
+      <div className="w-100 bg-priary-dark">
+        <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue">
+          {/* <div className="max-w-screen-lg px-4 py-8"> */}
+            <div className="text-end">
+              <h1 className="text-4xl font-bold mb-4">
+              <p>Hi there!</p>
+              <p>My name is Maciek</p>
+              <p>I’m a web developer</p>
 
 
-            </h1>
-            {/* move to next page img */}
-          </div>
+              </h1>
+              {/* move to next page img */}
+            </div>
+          {/* </div> */}
         </div>
       </div>
     </section>
