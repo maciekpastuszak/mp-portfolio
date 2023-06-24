8d8553b468b30c172154b1e7828e4a01e584b824
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-24 14:27:13 +0200

    update styles in hero text

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,10 +7,10 @@ const Hero = () => {
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue">
           {/* <div className="max-w-screen-lg px-4 py-8"> */}
             <div className="text-end">
-              <div>
-                <p className="font-light text-6xl">Hi there!</p>
-                <p className="font-light text-4xl">My name is Maciek</p>
-                <p className="font-light text-3xl">I’m a web developer</p>
+              <div className="grid gap-y-5">
+                <p className="font-medium text-6xl">Hi there!</p>
+                <p className="font-light text-4xl">My name is <span className="text-accent-yellow-300">Maciek</span></p>
+                <p className="font-extralight text-3xl">I’m a web developer</p>
               </div>
               {/* move to next page img */}
             </div>
