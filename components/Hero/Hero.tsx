4524ce63508edb2973522b022ef0301c496d7847
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-25 09:49:47 +0200

    correct Hero section colors and font styles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,13 +4,13 @@ const Hero = () => {
   return (
     <section>
       <div className="w-100 bg-primary-dark">
-        <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue">
+        <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300">
           {/* <div className="max-w-screen-lg px-4 py-8"> */}
             <div className="text-end">
-              <div className="grid gap-y-5">
+              <div className="grid gap-y-3">
                 <p className="font-medium text-6xl">Hi there!</p>
-                <p className="font-light text-4xl">My name is <span className="text-accent-yellow-300">Maciek</span></p>
-                <p className="font-extralight text-3xl">I’m a web developer</p>
+                <p className="font-normal text-4xl">My name is <span className="text-accent-yellow">Maciek</span></p>
+                <p className="font-light text-3xl">I’m a web developer</p>
               </div>
               {/* move to next page img */}
             </div>
