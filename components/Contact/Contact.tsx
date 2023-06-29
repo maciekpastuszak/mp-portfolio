eb085aeae686b2f72780551c679cc8ca0644de22
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-29 23:20:58 +0200

    add contact view

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+    <div className="w-100 bg-primary-dark">
+      <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300">
+        {/* <div className="max-w-screen-lg px-4 py-8"> */}
+          <div className="text-end">
+            <div className="grid gap-y-3">
+             Contact
+            </div>
+            {/* move to next page img */}
+          </div>
+        {/* </div> */}
+      </div>
+    </div>
+  </section>
   )
 }
 
