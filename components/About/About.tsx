40602c200dca677c05d170c839bedaa13b61a3af
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-29 23:18:24 +0200

    testing divs in about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,7 +5,12 @@ const About = () => {
     <section>
       <div className="min-h-screen flex items-center justify-center bg-accent-yellow text-secondary-blue">
         <div className="max-w-screen-lg px-4 py-8">
-
+          <div className="w-50 h-50 border-red-500">
+            Test
+          </div>
+          <div className="w-50 h-50 border-red-500">
+            Test
+          </div>
         </div>
       </div>
     </section>
