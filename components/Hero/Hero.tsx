0eac91a55c2bd2c9551ba835a4bcb825580e2519
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-07 09:25:28 +0200

    make background video full screen

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,52 +1,31 @@
-"use client"
-
 import React, { useEffect, useState } from 'react';
 
 const Hero = () => {
-  const [typedText, setTypedText] = useState('');
-  const firstLine = 'Heello there!';
-
-  useEffect(() => {
-    let currentIndex = 0;
-    const intervalId = setInterval(() => {
-      if (currentIndex === firstLine.length - 1) {
-        clearInterval(intervalId);
-      } else {
-        setTypedText((prevText) => prevText + firstLine[currentIndex]);
-        currentIndex++;
-      }
-    }, 100);
-  
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [firstLine]);
-
-
   return (
     <section>
       <div className="w-100 bg-primary-dark">
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute w-auto min-w-full min-h-full max-w-none opacity-10"
-      >
-        <source
-          src="backgroundVid.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300 z-10">
           <div className="text-end">
             <div className="grid gap-y-3">
-              <p className="font-medium text-6xl">{typedText}</p>
-              <p className="font-normal text-4xl">My name is <span className="text-accent-yellow">Maciek</span></p>
+              <p className="font-medium text-6xl">Hello there</p>
+              <p className="font-normal text-4xl">
+                My name is <span className="text-accent-yellow">Maciek</span>
+              </p>
               <p className="font-light text-3xl">I’m a web developer</p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="absolute inset-0">
+        <video
+          autoPlay
+          loop
+          muted
+          className="object-cover h-screen min-w-full max-h-full opacity-10 w-full"
+        >
+          <source src="backgroundVid.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </section>
   );
