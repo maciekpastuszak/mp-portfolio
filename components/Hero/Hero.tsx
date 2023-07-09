c77ee7571d5c6c3989c5c154ee72d3358861e74f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-09 12:30:15 +0200

    add link to Hero section to another page

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,19 +3,6 @@ import React, { useEffect, useState } from 'react';
 const Hero = () => {
   return (
     <section id="hero">
-      <div className="w-100 bg-primary-dark">
-        <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300 z-10">
-          <div className="text-end">
-            <div className="grid gap-y-3">
-              <p className="font-medium text-6xl">Hello there</p>
-              <p className="font-normal text-4xl">
-                My name is <span className="text-accent-yellow">Maciek</span>
-              </p>
-              <p className="font-light text-3xl">I’m a web developer</p>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="absolute inset-0">
         <video
           autoPlay
@@ -26,6 +13,26 @@ const Hero = () => {
           <source src="backgroundVid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+      </div>
+      <div className="w-100 bg-primary-dark">
+
+        {/* Hero header */}
+        <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300 z-10">
+          <div className="text-end">
+            <div className="grid gap-y-3">
+              <p className="font-medium text-6xl">Hello there</p>
+              <p className="font-normal text-4xl">
+                My name is <span className="text-accent-yellow">Maciek</span>
+              </p>
+              <p className="font-light text-3xl">I’m a web developer</p>
+            </div>
+
+            {/* Link to ABOUT */}
+            <div className="flex justify-center mt-5 absolute bottom-0">
+              About
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
