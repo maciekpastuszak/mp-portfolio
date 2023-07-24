34e2ed3f903777675c9561d413bdf5af29f18186
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-25 01:41:17 +0200

    add second paragraph typewriting effect

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TriangleLink from '../TriangleLink';
 import { AnimatePresence } from 'framer-motion';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
@@ -8,6 +8,10 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 const Hero = () => {
   const [text1] = useTypewriter({
     words: ['Hello there!']
+  });
+
+  const [text2, { isDone }] = useTypewriter({
+    words: [`My name is Maciek`]
   });
 
   return (
@@ -24,15 +28,21 @@ const Hero = () => {
         </video>
       </div>
       <div className="w-100 bg-primary-dark">
-
         {/* Hero header */}
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-end text-secondary-blue-300 z-10 text-end">
           <div className="grid gap-y-3">
             <p className="font-medium text-6xl">
               {text1}
+              <Cursor />
             </p>
-            <p className="font-normal text-4xl"> 
-            My name is <span className="text-accent-yellow">Maciek</span>  
+            <p className="font-normal text-4xl">
+              {isDone ? (
+                <>
+                  My name is <span className="text-accent-yellow">Maciek</span>
+                </>
+              ) : (
+                text2
+              )}
             </p>
             <p className="font-light text-3xl">I’m a web developer</p>
           </div>
