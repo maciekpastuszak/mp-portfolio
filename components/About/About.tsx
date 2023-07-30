9eb0ad7fd22696060476fc9563387650828a0c32
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-31 01:09:50 +0200

    new layout for strengths

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -6,10 +6,15 @@ const About = () => {
     <section id="about">
       <div className="w-100 bg-primary-dark">
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-center text-secondary-blue-300">
-          <div className="w-full justify-evenly border flex flex-col md:flex-row items-center">
-            <Strength />
-            <Strength />
-            <Strength />
+          <div className="w-full justify-center border flex flex-col sm:flex-row items-center">
+            <div className='flex flex-col lg:flex-row'>
+              <Strength />
+              <Strength />
+            </div>
+            <div className='flex flex-col lg:flex-row'>
+              <Strength />
+              <Strength />
+            </div>
           </div>
         </div>
       </div>
