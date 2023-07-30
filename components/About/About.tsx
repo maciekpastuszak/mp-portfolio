32d1f2659eedbb859de22022e9260e0730057cfe
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-30 23:27:25 +0200

    add rremaining testing strengths

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -6,9 +6,10 @@ const About = () => {
     <section id="about">
       <div className="w-100 bg-primary-dark">
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-center text-secondary-blue-300">
-          <div className="w-full justify-between border">
+          <div className="w-full justify-evenly border flex flex-col md:flex-row items-center">
             <Strength />
-            
+            <Strength />
+            <Strength />
           </div>
         </div>
       </div>
