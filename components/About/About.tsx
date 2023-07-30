3be1b4a72586d054fa4b2fb1c88aae1770c531ea
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-30 22:58:22 +0200

    add pros wrapper

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,7 +5,9 @@ const About = () => {
     <section id="about">
       <div className="w-100 bg-primary-dark">
         <div className="min-h-screen mx-auto w-5/6 flex items-center justify-center text-secondary-blue-300">
-
+          <div className="w-full justify-between border">
+          
+          </div>
         </div>
       </div>
     </section>
