117b31f41ced9eab875c1ca34816687944f9c2b8
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-30 23:11:01 +0200

    add strength component

---
+++ components/Strength.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Strength = () => {
+  return (
+    <>
+        <div className="w-[200px] h-[200px] border">
+            Test
+        </div>
+    </>
+  )
+}
+
+export default Strength
