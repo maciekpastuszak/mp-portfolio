c6a6ab5551e194df4082ef5c401fcfb6947613c1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-31 09:20:07 +0200

    add strength card const data

--- constants/index.tsx
+++ constants/index.tsx
@@ -3,3 +3,21 @@ export const NavLinks = [
     { href: '/#portfolio', key: 'portfolio', text: 'Portfolio' },
     { href: '/#contact', key: 'contact', text: 'Contact' },
   ];
+
+export const StrengthCard = [
+  {
+    src: '/polygon1.svg',
+    title: 'Problem solver',
+    description: 'Thrive on finding creative solutions to complex challenges in order to achieve optimal results'
+  },
+  {
+    src: '/polygon2.svg',
+    title: 'Innovative',
+    description: 'Constantly exploring new technologies and pushing the boundaries of web development'
+  },
+  {
+    src: '/polygon3.svg',
+    title: 'Continuous learning',
+    description: 'Committed to staying updated with the latest trends and best practices in front-end development.'
+  },
+];
