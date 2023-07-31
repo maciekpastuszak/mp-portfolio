854b4121db220857f54ee635dcd79ed72b1d01de
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-07-31 08:43:21 +0200

    delete TriangleLink

--- components/Strength.tsx
+++ components/Strength.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Strength = () => {
   return (
     <>
-        <div className="m-3 p-3 w-[200px] h-[200px] text-center border">
-            <img src="polygon1.svg" alt="polygon1" />
+        <div className="m-3 p-3 w-[200px] h-[200px] items-center border">
+            <Image 
+            src="polygon1.svg" 
+            width={89} 
+            height={82} 
+            alt="polygon1" />
+            <h2>Problem solver</h2>
+            <p>Thrive on finding creative solutions 
+to complex challenges 
+in order to achieve optimal results</p>
         </div>
     </>
   )
