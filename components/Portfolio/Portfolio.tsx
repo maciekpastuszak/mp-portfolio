18f5d99b3f8d4296e07c7ec328fb57abec627bad
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-08-24 00:52:18 +0200

    bring back porfolio page

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -8,7 +8,13 @@ type Props = {
 const Portfolio = ({ setSelectedPage }: Props) => {
   return (
     <section id="portfolio">
-      portfolio
+    <div className="min-h-screen flex items-center justify-center bg-red-500 text-secondary-blue">
+      <div className="max-w-screen-lg px-4 py-8">
+        <div className="w-50 h-50 border-red-500">
+          Test
+        </div>
+      </div>
+    </div>
   </section>
   )
 }
