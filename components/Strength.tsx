6b26aa42a42c2e17cc3d833b5aaa12e066249371
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-08-28 22:41:07 +0200

    fix chrome console error regarding Image

--- components/Strength.tsx
+++ components/Strength.tsx
@@ -10,17 +10,19 @@ interface StrengthProps {
 const Strength: React.FC<StrengthProps> = ({icon, title, content}) => {
   return (
     <>
-        <div className="m-3 p-3 w-[200px] h-[250px] items-center justify-center text-center border border-transparent flex flex-col">
+        <div className="m-3 p-3 w-[200px] h-[250px] items-center justify-center text-center border border-transparent flex flex-col" >
             <Image 
             src={icon}
             width={89} 
             height={82} 
-            alt={title} />
+            alt={title} 
+            style={{ width: 36, height: 36 }}
+            />
             <h2 className="font-bold my-2">{title}</h2>
             <p className="text-sm font-light">{content}</p>
         </div>
     </>
-  )
+  );
 }
 
 export default Strength
