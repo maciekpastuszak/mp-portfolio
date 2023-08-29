38a387b0d25fc9d266724e1f4bb8dd4a21f4f999
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-08-29 18:31:14 +0200

    map through portfolio items in the items gallery

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,5 +1,6 @@
 import { SelectedPage } from '@/shared/types';
 import React from 'react'
+import PortfolioItem from '../PortfolioItem';
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -8,9 +9,11 @@ type Props = {
 const Portfolio = ({ setSelectedPage }: Props) => {
   return (
     <section id="portfolio" className="min-h-screen bg-primary-dark">
-      <div className="mx-auto w-5/6 flex items-center  border border-red-400">
-        <div className="container flex flex-row wrap">
-        <p className="text-white">test</p>
+      <div className="mx-auto w-5/6 py-12 border border-red-400">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {portfolioItems.map((item, index) => (
+            <PortfolioItem key={index} />
+          ))}
         </div>
       </div>
     </section>
