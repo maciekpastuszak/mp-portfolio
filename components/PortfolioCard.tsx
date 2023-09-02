fce62b370d85b5eb6d64a88051bf28ee587b5c0f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-02 14:26:09 +0200

    add PortfolioCard props

--- components/PortfolioCard.tsx
+++ components/PortfolioCard.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { portfolioItems } from '@/constants';
+import React from 'react';
 
-const PortfolioCard= () => {
+type Props = {
+  webUrl: string,
+  codeUrl: string,
+  imgSrc: string,
+  title: string,
+  description: string
+};
+
+const PortfolioCard= ({webUrl, codeUrl, imgSrc, title, description} : Props) => {
   return (
     <div className="w-48 h-48 bg-gray-800 m-4">
         PortfolioCard
