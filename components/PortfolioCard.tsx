439dfc224b5a36a4aed149213594438bc3c7529b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-03 12:19:14 +0200

    basic setup for mapping through porfolio cards

--- components/PortfolioCard.tsx
+++ components/PortfolioCard.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 const PortfolioCard= ({webUrl, codeUrl, imgSrc, title, description} : Props) => {
   return (
-    <div className="w-48 h-48 bg-gray-800 m-4">
-        PortfolioCard
+    <div className="w-[716px] h-[345] m-4">
+        <img src={imgSrc} alt={title} />
     </div>
   )
 }
