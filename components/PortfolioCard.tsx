3ac0bfa05cd648d59f2bc40b4c34cd957bd0a742
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-04 00:57:22 +0200

    add buttons to the PorfolioCard

--- components/PortfolioCard.tsx
+++ components/PortfolioCard.tsx
@@ -19,15 +19,27 @@ const PortfolioCard= ({webUrl, codeUrl, imgSrc, title, description} : Props) => 
   }
 
   return (
-    <div className="
-      w-[400px] 
-      h-[200] 
-      transition-opacity 
-      duration-300 
-      ease-in 
-      hover:opacity-25">
+    <div 
+      className="
+        w-[400px] 
+        h-[200] 
+        transition-opacity 
+        duration-300 
+        ease-in 
+        hover:opacity-25"
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}>
         <img src={imgSrc} alt={title} />
-        <button></button>
+        {hovered && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 transition-opacity duration-300 ease-in opacity-0 hover:opacity-100">
+          <button className="bg-blue-500 text-white px-4 py-2 m-2 hover:bg-blue-600 z-10">
+            Code
+          </button>
+          <button className="bg-green-500 text-white px-4 py-2 m-2 hover:bg-green-600 z-10">
+            Link
+          </button>
+        </div>
+      )}
     </div>
   )
 }
