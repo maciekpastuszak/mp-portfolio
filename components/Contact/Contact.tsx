5ed3801585d255a32c1e04c6ff5ddb60d8a5538d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-09 21:22:47 +0200

    add basic form with name to Contact

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -9,7 +9,14 @@ const Contact = ({ setSelectedPage }: Props) => {
   return (
     <section id="contact" className="w-full min-h-screen bg-primary-dark">
       <div className="w-5/6 bg-primary-dark py-24 text-white flex mx-auto justify-center items-center"> 
-       <p>Kontakt</p>
+       <form action="">
+
+          <div className="w-full flex flex-col">
+            <label htmlFor="Name">Name</label>
+            <input type="text" id="name" autoComplete="off" />
+          </div>
+
+       </form>
       </div>
   </section>
   )
