45265fcdf65c89d5c0f6e10ee50592620e435b89
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-11 00:40:59 +0200

    update button to introduce disabled state

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -82,7 +82,7 @@ const Contact = ({ setSelectedPage }: Props) => {
               maxLength={500} 
               required/>
           </div>
-          <button type="submit" className="w-[127px] h-[44px] shrink-0 rounded-xl border-2 border-accent-yellow-800 hover:bg-accent-yellow-800 hover:text-primary-dark-800">send</button>
+          <button type="submit" disabled={loading} className="w-[127px] h-[44px] shrink-0 disabled:bg-gray-400 disabled:text-gray-100 rounded-xl border-2 border-accent-yellow-800 hover:bg-accent-yellow-800 hover:text-primary-dark-800">send</button>
        </form>
       </div>
   </section>
