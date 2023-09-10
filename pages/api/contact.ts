aa26fc6ed089d3eb593b0032cf33bf779d2dd52f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-10 22:36:43 +0200

    add transporter to send the mail

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -4,5 +4,24 @@ export default async function (req: Request,res: Response) {
 
     const { name, email, message } = req.body;
 
-    return req.status(200).json({message: "success"})
+    const data = {
+        name, email, message
+    }
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 456,
+        secure: true,
+        auth: {
+            user: process.env.user,
+            pass: process.env.pass,
+        },
+    });
+
+    try {
+        return req.status(200).json({message: "success"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Could not send the email. Your message was not sent"})
+    }
 }
