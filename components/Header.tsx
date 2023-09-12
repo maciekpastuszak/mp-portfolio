775332fe7b47212e33f024583f9439e4694dd134
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-13 00:09:41 +0200

    adjust icons in the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IconContext } from "react-icons";
 import {FaChevronDown, FaChevronUp} from 'react-icons/fa';
 
 type HeaderProps = {
@@ -7,12 +8,18 @@ type HeaderProps = {
 
 const Header = ({title} : HeaderProps) => {
   return (
-    <div className="relative flex flex-col justify-start text-white font-bold text-3xl">
-      <FaChevronUp />
+    <div className="relative flex flex-col justify-center items-center text-white font-bold text-3xl my-3">
+      <IconContext.Provider value={{ className: "text-accent-yellow font-thin" }}>
+        <div>
+          <FaChevronUp />
+        </div>
         {title.toUpperCase()}
         <div className="absolute top-10 left-0 w-full justify-start">
       </div>
-      <FaChevronDown />
+      <div>
+          <FaChevronDown />
+        </div>
+      </IconContext.Provider>
     </div>
   )
 }
