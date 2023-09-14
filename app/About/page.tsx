cff45f6253a4efb7bec5375f224ea7fad112729d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-14 09:11:35 +0200

    add gradient to About page

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -13,7 +13,7 @@ type Props = {
 const About = ({ setSelectedPage }: Props) => {
 
   return (
-    <section id="about" className="min-h-screen w-full py-8 bg-primary-dark">
+    <section id="about" className="min-h-screen w-full py-8 bg-primary-dark gradient-background">
         <div className="relative inset-x-0 top-16 min-h-screen mx-auto w-5/6 flex items-start text-secondary-blue-300">
           <div className="flex flex-col w-full justify-center items-center">
             <div className="relative flex flex-col sm:flex-row justify-center items-center">
@@ -32,7 +32,7 @@ const About = ({ setSelectedPage }: Props) => {
               </div>
             </div>
 
-            <div className="flex text-center mt-8 mb-4">
+            <div className="flex text-center mt-8 mb-3">
               <Header title="Strengths" />
             </div>
 
