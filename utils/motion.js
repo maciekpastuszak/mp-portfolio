3faa7a5c95a092b8fbb8ac81154e1f680b35f92e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-15 09:43:44 +0200

    modify motion config

--- utils/motion.js
+++ utils/motion.js
@@ -36,28 +36,7 @@ export const navVariants = {
     },
   });
 
-  export const fadeIn = {
-    hidden: {
-      opacity: 0,
-      y: -40,
-      transition: {
-        type: 'spring',
-        stiffness: 300,
-        damping: 140,
-      },
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: 'spring',
-        stiffness: 80,
-        delay: 2,
-      },
-    },
-  };
-
-  export const rollIn = (direction, type, delay, duration) => ({
+  export const fadeIn = (direction, type, delay, duration) => ({
     hidden: {
       x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
       y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
