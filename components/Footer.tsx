9352c41c01ce250593b014876ae86a8306fe7751
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-16 22:22:17 +0200

    update icons size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
             <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="#about">About</a></p>
             <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="#portfolio">Portfolio</a></p>
             <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="#contact">Contact</a></p>
-            <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="https://github.com/maciekpastuszak" target="_blank" rel="noopener noreferrer"><BsGithub size={25} /></a></p>
-            <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="#contact" target="_blank" rel="noopener noreferrer"><BsLinkedin size={25} /></a></p>
+            <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="https://github.com/maciekpastuszak" target="_blank" rel="noopener noreferrer"><BsGithub size={22} /></a></p>
+            <p className="text-secondary-blue-300 hover:drop-shadow-md text-sm my-5 mx-5"><a href="#contact" target="_blank" rel="noopener noreferrer"><BsLinkedin size={22} /></a></p>
         </div>
     <div className="mt-16 basis-1/4 md:mt-0">
     </div>
