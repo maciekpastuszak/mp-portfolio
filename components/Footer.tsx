a2ab05b78ac7a1b2bbd68a94503df682f0bb6358
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-16 22:15:58 +0200

    update footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-dark-800 py-16">
-        <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
-            <div className="mt-16 basis-1/2 md:mt-0">
-                <a href="#about" className="text-secondary-blue-300 text-sm my-5">About me</a>
-                <a href="#portfolio" className="text-secondary-blue-300 text-sm my-5">Portfolio</a>
-                <a href="#contact" className="text-secondary-blue-300 text-sm my-5">Contact</a>
-            </div>
-            <div className="mt-16 basis-1/4 md:mt-0">
-
-            </div>
+<footer className="bg-primary-dark-800 py-16">
+    <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+            <p className="text-secondary-blue-300 text-sm my-5"><a href="#">Home</a></p>
+            <p className="text-secondary-blue-300 text-sm my-5"><a href="#about">About me</a></p>
+            <p className="text-secondary-blue-300 text-sm my-5"><a href="#portfolio">Portfolio</a></p>
+            <p className="text-secondary-blue-300 text-sm my-5"><a href="#contact">Contact</a></p>
         </div>
-    </footer>
+    <div className="mt-16 basis-1/4 md:mt-0">
+    </div>
+    <div className="mt-16 basis-1/4 md:mt-0">
+    </div>
+    </div>
+</footer>
+
+
   )
 }
 
