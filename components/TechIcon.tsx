0c3df62ae64c550d76058681044a935c615f8768
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-17 18:20:01 +0200

    add tooltip to Techicons

--- components/TechIcon.tsx
+++ components/TechIcon.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import ReactTooltip from 'react-tooltip';
 
 interface TechIconProps {
     src: string,
@@ -9,13 +10,20 @@ interface TechIconProps {
 const TechIcon: React.FC<TechIconProps> = ({src, title}) => {
   return (
     <>
+     <div
+        data-tip={title}
+        data-place="bottom"
+        data-effect="solid"
+      >
         <Image 
             src={src}
             alt={title}
             title={title}
             width={80}
             height={80}
+            className="hover:drop-shadow-md"
         />
+      </div>
     </>
   )
 }
