7670fd30aee2198ae2c6097ef4adcac0e5a863cc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-17 15:47:05 +0200

    update animation on About

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { SelectedPage } from '@/shared/types';
 import Header from '../../components/Header';
 import { AnimatePresence, motion } from 'framer-motion';
+import { staggerContainer, fadeIn } from '@/utils/motion';
 
 
 type Props = {
@@ -17,26 +18,35 @@ const About = ({ setSelectedPage }: Props) => {
   return (
     <section id="about" className="min-h-screen w-full py-8 bg-primary-dark gradient-aboutSection-top">
         <div className="relative inset-x-0 top-16 min-h-screen mx-auto w-5/6 flex items-start text-secondary-blue-300">
-          <div className="flex flex-col w-full justify-center items-center">
+          <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className="flex flex-col w-full justify-center items-center">
 
           <div className="flex text-center mb-4">
               <Header title="About me" />
             </div>
 
             <div className="relative flex flex-col sm:flex-row justify-center items-center">
-              <div className='flex flex-col lg:flex-row sm:w-1/2 w-full h-auto'>
+              <motion.div 
+                variants={fadeIn('right', 'tween', 0.2, 1)}
+                className='flex flex-col lg:flex-row sm:w-1/2 w-full h-auto'>
                 <p className="leading-relaxed text-secondary-blue text-sm lg:text-base text-center sm:text-end md:pe-4 mt-5 mb-5">Highly accomplished manager and web developer with over 15 years 
                   of experience leading teams and delivering successful 
                   projects. Skilled in strategic planning, team building, and project management, 
                   as well as in modern web development technologies.</p>
-              </div>
+              </motion.div>
               <div>
                 <img src="/MyImage.png" alt="PortfolioImage" className="w-[250px] h-[250px] max-w-fit" />
               </div>
-              <div className='flex flex-col lg:flex-row sm:w-1/2 w-full h-auto'>
+              <motion.div 
+                variants={fadeIn('left', 'tween', 0.2, 1)}
+                className='flex flex-col lg:flex-row sm:w-1/2 w-full h-auto'>
                 <p className="leading-relaxed text-secondary-blue text-sm lg:text-base text-center sm:text-start md:ps-4 mt-5 mb-5"> A creative problem-solver 
                   with a strong work ethic and a passion for technology. My commitment to continuous learning and innovation ensures that I consistently deliver cutting-edge solutions to clients and exceed their expectations.</p>
-              </div>
+              </motion.div>
             </div>
             <div className="grad-aboutSection-rc z-0" />
 
@@ -74,7 +84,7 @@ const About = ({ setSelectedPage }: Props) => {
               </div>
             </div>
 
-          </div>
+          </motion.div>
         </div>
     </section>
   )
