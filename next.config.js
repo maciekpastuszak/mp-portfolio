27414ae3d05017bcf6b833b3e6dfdef68d229173
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-09-18 10:04:43 +0200

    modify browser config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
+    images: {
+      loader: 'custom',
+      loaderFile: './pages/api/custom-loader.ts',
+    }
   }
-
-module.exports = nextConfig
+   
+  module.exports = nextConfig
