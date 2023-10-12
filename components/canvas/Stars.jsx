90ca951da9fb1493b90f289d7cc918e0caa680bc
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-12 10:39:55 +0200

    add stars rotation

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -10,7 +10,7 @@ const Stars = (props) => {
   const sphere = random.inSphere(new Float32Array(5000), {radius: 1.2 })
 
   return (
-    <group>
+    <group rotation={[0, 0, Math.PI / 4]}>
     <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
       <PointMaterial
         transparent
