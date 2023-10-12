763d52298a35d90017558a893c200409ca272951
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-12 10:43:38 +0200

    update stars rotation

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -7,7 +7,12 @@ import * as random from 'maath/random/dist/maath-random.esm'
 const Stars = (props) => {
   const ref = useRef();
 
-  const sphere = random.inSphere(new Float32Array(5000), {radius: 1.2 })
+  const [sphere] = random.inSphere(new Float32Array(5000), {radius: 1.2 })
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
