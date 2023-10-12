1e626bca2632ca541c0855978e68dc61d002da6c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-12 10:34:40 +0200

    add group of points and update imports

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -1,14 +1,18 @@
-import React, { Suspense, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {Points, PointMaterial, Preload } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
+import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
   const ref = useRef();
 
+  const sphere = random.inSphere(new Float32Array(5000), {radius: 1.2 })
+
   return (
-    <div>Stars</div>
+    <group>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled/>
+    </group>
   )
 }
 
