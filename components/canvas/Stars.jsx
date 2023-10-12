f090a3f6489e9dbe1c38bf10a5007b3bb704b97a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-12 09:59:39 +0200

    typo

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -13,9 +13,9 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-full absolute inset-0 z-[-1]">
-      <canvas>
-        
-      </canvas>
+      <Canvas>
+
+      </Canvas>
     </div>
   )
 }
