f273af9265881c3441e62a06453c7ee9d55bb650
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-04 23:49:14 +0200

    Refactor constants and add new SVG icons

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,160 +1,183 @@
 export const navLinks = [
-    { href: '/#about', key: 'about', text: 'About' },
-    { href: '/#portfolio', key: 'portfolio', text: 'Portfolio' },
-    { href: '/#contact', key: 'contact', text: 'Contact' },
-  ];
+  { href: "/#about", key: "about", text: "About" },
+  { href: "/#portfolio", key: "portfolio", text: "Portfolio" },
+  { href: "/#contact", key: "contact", text: "Contact" },
+];
 
 export const strengthCards = [
   {
-    src: '/polygon1.svg',
-    title: 'Problem solver',
-    description: 'Thrive on finding creative solutions to complex challenges in order to achieve optimal results'
+    src: "/polygon1.svg",
+    title: "Problem solver",
+    description:
+      "Thrive on finding creative solutions to complex challenges in order to achieve optimal results",
   },
   {
-    src: '/polygon2.svg',
-    title: 'Innovative',
-    description: 'Constantly exploring new technologies and pushing the boundaries of modern web development'
+    src: "/polygon2.svg",
+    title: "Innovative",
+    description:
+      "Constantly exploring new technologies and pushing the boundaries of modern web development",
   },
   {
-    src: '/polygon3.svg',
-    title: 'Continuous learning',
-    description: 'Committed to staying updated with the latest trends and best practices in front-end development.'
+    src: "/polygon3.svg",
+    title: "Continuous learning",
+    description:
+      "Committed to staying updated with the latest trends and best practices in front-end development.",
   },
   {
     index: 4,
-    src: '/polygon4.svg',
-    title: 'User-focused',
-    description: 'Passionate about creating web experiences that prioritize user needs and preferences.'
+    src: "/polygon4.svg",
+    title: "User-focused",
+    description:
+      "Passionate about creating web experiences that prioritize user needs and preferences.",
   },
 ];
 
 export const techIcons = [
   {
-    src: '/icon-atlassian.svg',
-    title: 'Atlassian'
+    src: "/icon-next.svg",
+    title: "NextJS",
   },
   {
-    src: '/icon-csharp.svg',
-    title: 'C#'
+    src: "/icon-react.svg",
+    title: "ReactJS",
   },
   {
-    src: '/icon-css.svg',
-    title: 'CSS'
+    src: "/icon-csharp.svg",
+    title: "C#",
   },
   {
-    src: '/icon-express.svg',
-    title: 'ExpressJS'
+    src: "/icon-aspnet.svg",
+    title: "asp.net MVC",
   },
   {
-    src: '/icon-git.svg',
-    title: 'GIT'
+    src: "/icon-typescript.svg",
+    title: "TypeScript",
   },
   {
-    src: '/icon-html.svg',
-    title: 'HTML'
+    src: "/icon-html.svg",
+    title: "HTML",
   },
   {
-    src: '/icon-js.svg',
-    title: 'JavaScript'
+    src: "/icon-css.svg",
+    title: "CSS",
   },
   {
-    src: '/icon-mongo.svg',
-    title: 'mongoDB'
+    src: "/icon-js.svg",
+    title: "JavaScript",
   },
   {
-    src: '/icon-next.svg',
-    title: 'NextJS'
+    src: "/icon-tailwind.svg",
+    title: "TailwindCSS",
   },
   {
-    src: '/icon-node.svg',
-    title: 'nodeJS'
+    src: "/icon-bootstrap.svg",
+    title: "Bootstrap",
   },
   {
-    src: '/icon-openai.svg',
-    title: 'OpenAI'
+    src: "/icon-jquery.svg",
+    title: "jQuery",
   },
   {
-    src: '/icon-python.svg',
-    title: 'Python'
+    src: "/icon-node.svg",
+    title: "nodeJS",
   },
   {
-    src: '/icon-react.svg',
-    title: 'ReactJS'
+    src: "/icon-express.svg",
+    title: "ExpressJS",
   },
   {
-    src: '/icon-solidity.svg',
-    title: 'Solidity'
+    src: "/icon-git.svg",
+    title: "GIT",
   },
   {
-    src: '/icon-mysql.svg',
-    title: 'MySQL'
+    src: "/icon-openai.svg",
+    title: "OpenAI",
   },
   {
-    src: '/icon-tailwind.svg',
-    title: 'TailwindCSS'
+    src: "/icon-python.svg",
+    title: "Python",
   },
   {
-    src: '/icon-typescript.svg',
-    title: 'TypeScript'
+    src: "/icon-solidity.svg",
+    title: "Solidity",
   },
-]
+  {
+    src: "/icon-mysql.svg",
+    title: "MySQL",
+  },
+  {
+    src: "/icon-mongo.svg",
+    title: "mongoDB",
+  },
+  {
+    src: "/icon-figma.svg",
+    title: "Figma",
+  },
+  {
+    src: "/icon-atlassian.svg",
+    title: "Atlassian",
+  },
+];
 
 export const portfolioItems = [
   {
-    webUrl: 'https://karolinapastuszak.pl',
-    codeUrl: 'https://github.com/maciekpastuszak/karolina-photo',
-    imgSrc: '/screen_karolina_photo.jpg',
-    title: 'Karolina Photography',
-    description: "A porfolio website of a family photographer"
+    webUrl: "https://karolinapastuszak.pl",
+    codeUrl: "https://github.com/maciekpastuszak/karolina-photo",
+    imgSrc: "/screen_karolina_photo.jpg",
+    title: "Karolina Photography",
+    description: "A porfolio website of a family photographer",
   },
   {
-    webUrl: 'https://dobrawrozka.edu.pl',
-    codeUrl: 'https://github.com/maciekpastuszak/dobra-wrozka-v2',
-    imgSrc: '/screen_dobra_wrozka.jpg',
-    title: 'Dobra Wrózka',
-    description: "A kindergarten website"
+    webUrl: "https://dobrawrozka.edu.pl",
+    codeUrl: "https://github.com/maciekpastuszak/dobra-wrozka-v2",
+    imgSrc: "/screen_dobra_wrozka.jpg",
+    title: "Dobra Wrózka",
+    description: "A kindergarten website",
   },
   {
-    webUrl: 'http://evo-gym.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/gym-typescript',
-    imgSrc: '/screen_evo_gym.jpg',
-    title: 'Evo Gym',
-    description: "A modern and elegant UI/UX fitness center front page"
+    webUrl: "http://evo-gym.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/gym-typescript",
+    imgSrc: "/screen_evo_gym.jpg",
+    title: "Evo Gym",
+    description: "A modern and elegant UI/UX fitness center front page",
   },
   {
-    webUrl: 'https://gericht-restaurant.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/Gerich-Restaurant',
-    imgSrc: '/screen_gericht.jpg',
-    title: 'Gericht Restaurant',
-    description: "A beautiful and modern restaurant front page"
+    webUrl: "https://gericht-restaurant.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/Gerich-Restaurant",
+    imgSrc: "/screen_gericht.jpg",
+    title: "Gericht Restaurant",
+    description: "A beautiful and modern restaurant front page",
   },
   {
-    webUrl: 'https://gpt3.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/GPT3-Technology',
-    imgSrc: '/screen_gpt3.jpg',
-    title: 'GPT-3 Technology',
-    description: "Modern UI/UX GPT-3 landing page based on react with BEM CSS"
+    webUrl: "https://gpt3.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/GPT3-Technology",
+    imgSrc: "/screen_gpt3.jpg",
+    title: "GPT-3 Technology",
+    description: "Modern UI/UX GPT-3 landing page based on react with BEM CSS",
   },
   {
-    webUrl: 'https://twitter-clone-5gxphh679-maciekpastuszak.vercel.app/',
-    codeUrl: 'https://github.com/maciekpastuszak/twitter-clone',
-    imgSrc: '/screen_twitter.jpg',
-    title: 'Twitter Clone',
-    description: "A Twitter clone using React, Next.js, TailwindCSS, Prisma and mongoDB"
+    webUrl: "https://twitter-clone-5gxphh679-maciekpastuszak.vercel.app/",
+    codeUrl: "https://github.com/maciekpastuszak/twitter-clone",
+    imgSrc: "/screen_twitter.jpg",
+    title: "Twitter Clone",
+    description:
+      "A Twitter clone using React, Next.js, TailwindCSS, Prisma and mongoDB",
   },
   {
-    webUrl: 'http://dallee.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/dallee',
-    imgSrc: '/screen_dallee.jpg',
-    title: 'Dallee',
-    description: "A social media platform to create and share DALL-E generated images"
+    webUrl: "http://dallee.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/dallee",
+    imgSrc: "/screen_dallee.jpg",
+    title: "Dallee",
+    description:
+      "A social media platform to create and share DALL-E generated images",
   },
   {
-    webUrl: 'https://expo.dev/@paciekfly/react_native_jobs?serviceType=classic&distribution=expo-go',
-    codeUrl: 'https://github.com/maciekpastuszak/react_native_jobs',
-    imgSrc: '/screen_jobs.jpg',
-    title: 'Jobs',
-    description: "A React Native job search application"
+    webUrl:
+      "https://expo.dev/@paciekfly/react_native_jobs?serviceType=classic&distribution=expo-go",
+    codeUrl: "https://github.com/maciekpastuszak/react_native_jobs",
+    imgSrc: "/screen_jobs.jpg",
+    title: "Jobs",
+    description: "A React Native job search application",
   },
   // {
   //   webUrl: 'http://flexibble.maciejpastuszak.pl/',
@@ -164,18 +187,18 @@ export const portfolioItems = [
   //   description: ""
   // },
   {
-    webUrl: 'https://promptopia.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/promptopia',
-    imgSrc: '/screen_promptopia.jpg',
-    title: 'Promptopia',
-    description: "A reliable OpenAI prompts sharing platform"
+    webUrl: "https://promptopia.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/promptopia",
+    imgSrc: "/screen_promptopia.jpg",
+    title: "Promptopia",
+    description: "A reliable OpenAI prompts sharing platform",
   },
   {
-    webUrl: 'https://spotifyAI.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/SpotifyAI',
-    imgSrc: '/screen_spotifyai.jpg',
-    title: 'SpotifyAI',
-    description: "An AI powered playlist generator"
+    webUrl: "https://spotifyAI.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/SpotifyAI",
+    imgSrc: "/screen_spotifyai.jpg",
+    title: "SpotifyAI",
+    description: "An AI powered playlist generator",
   },
   // {
   //   webUrl: 'https://codereviewer.maciejpastuszak.pl/',
@@ -185,39 +208,41 @@ export const portfolioItems = [
   //   description: ""
   // },
   {
-    webUrl: 'https://color-find.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/color-palette-generator',
-    imgSrc: '/screen_color_find.jpg',
-    title: 'ColorFind',
-    description: "A color palette generator usign OpenAi"
+    webUrl: "https://color-find.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/color-palette-generator",
+    imgSrc: "/screen_color_find.jpg",
+    title: "ColorFind",
+    description: "A color palette generator usign OpenAi",
   },
   {
-    webUrl: 'https://krypt.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/Krypt',
-    imgSrc: '/screen_krypt.jpg',
-    title: 'Krypt',
-    description: "A Web3.0/Ethereum blockchain app for cryptocurrency transfer."
+    webUrl: "https://krypt.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/Krypt",
+    imgSrc: "/screen_krypt.jpg",
+    title: "Krypt",
+    description:
+      "A Web3.0/Ethereum blockchain app for cryptocurrency transfer.",
   },
   {
-    webUrl: 'https://yelpcamp.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/YelpCamp',
-    imgSrc: '/screen_yelpcamp.jpg',
-    title: 'YelpCamp',
-    description: "My first Web Developer Bootcamp application"
+    webUrl: "https://yelpcamp.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/YelpCamp",
+    imgSrc: "/screen_yelpcamp.jpg",
+    title: "YelpCamp",
+    description: "My first Web Developer Bootcamp application",
   },
   {
-    webUrl: 'https://hangman.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/React-Hangman',
-    imgSrc: '/screen_hangman.jpg',
-    title: 'React Hangman',
-    description: "A simple react-typescript app based on a popular children game"
+    webUrl: "https://hangman.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/React-Hangman",
+    imgSrc: "/screen_hangman.jpg",
+    title: "React Hangman",
+    description:
+      "A simple react-typescript app based on a popular children game",
   },
   {
-    webUrl: 'https://hoobank.maciejpastuszak.pl/',
-    codeUrl: 'https://github.com/maciekpastuszak/bank_modern_app',
-    imgSrc: '/screen_hoobank.jpg',
-    title: 'Hoo Bank',
-    description: "a modern and elegant UI/UX business front page"
+    webUrl: "https://hoobank.maciejpastuszak.pl/",
+    codeUrl: "https://github.com/maciekpastuszak/bank_modern_app",
+    imgSrc: "/screen_hoobank.jpg",
+    title: "Hoo Bank",
+    description: "a modern and elegant UI/UX business front page",
   },
   // {
   //   webUrl: 'https://dsurv.maciejpastuszak.pl/',
@@ -226,4 +251,4 @@ export const portfolioItems = [
   //   title: 'dsurv',
   //   description: "my first dfinity token application"
   // }
-]
+];
